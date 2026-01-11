64ccedb9ecc2dbc75a72546b5d3516f2b134f183
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:37:02 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "synapse_sql_pool_extended_auditing_policys" {
+  description = "All synapse_sql_pool_extended_auditing_policy resources"
+  value       = azurerm_synapse_sql_pool_extended_auditing_policy.synapse_sql_pool_extended_auditing_policys
+}
+
